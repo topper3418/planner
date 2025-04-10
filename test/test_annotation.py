fa7ef4610a05097d3ff623a7eab362ff0d5589b5
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-04-10 00:15:05 -0500

    more robust test, need to tune to make it pass.

--- test/test_annotation.py
+++ test/test_annotation.py
@@ -73,3 +73,68 @@ def test_process_note(initial_note):
     assert category is not None
     assert category.name == "Action"
     
+
+notes_categories = {
+    "Action": [
+        "I am finished with work for the day",
+        "I pushed my commit",
+        "I am going to bed",
+        "I am taking a break",
+        "I am starting on my garden project",
+        "I found and fixed the bug",
+        "I am going to the gym",
+        "Starting on my project",
+        "talked to the wife for a few minutes"
+    ],
+    "Todo": [
+        "I need to finish my project",
+        "I will talk to the vendor",
+        "The truck needs its oil changed",
+        "I need to call my mom",
+        "I need to work out today", 
+        "I need to do laundry",
+        "I need to clean the house"
+    ],
+    "Curiosity": [
+        "I wonder how many cities there are in the US",
+        "Can I use asserts in a pytest fixture?",
+        "I wonder how many people are in the world",
+        "How do I do tests in golang?"
+    ],
+    "Observation": [
+        "I noticed there is a new restaurant in town",
+        "The tomatoes are ripe",
+        "Today is a sunny day",
+        "10 clients crashed the server on a raspberry pi zero W",
+        "The truck had its SRS light on today"
+    ]
+}
+
+def test_process_notes():
+    # create a test note to work with
+    for category_name, notes in notes_categories.items():
+        success_obj = {
+            "success": 0,
+            "fail": 0
+        }
+        success_rate = {
+            "Action": success_obj.copy(),
+            "Todo": success_obj.copy(),
+            "Curiosity": success_obj.copy(),
+            "Observation": success_obj.copy()
+        }
+        for note_text in notes:
+            note = db.Note.create(note_text)
+            category = processor.categorize_note(note)
+            assert category is not None
+            if category.name == category_name:
+                success_rate[category_name]["success"] += 1
+            else:
+                print(f"Failed to categorize note: {note_text}. Expected {category_name}, got {category.name}")
+                success_rate[category_name]["fail"] += 1
+        assert success_rate["Action"]["fail"] == 0
+        assert success_rate["Todo"]["fail"] == 0
+        assert success_rate["Curiosity"]["fail"] == 0
+        assert success_rate["Observation"]["fail"] == 0
+
+
