314c7e76967516995e684609ffae13d148fc269e
topper3418 <travisopperud@gmail.com>
2025-05-10 00:26:51 -0500

    added run_engine back

---
+++ run_engine.py
@@ -0,0 +1,12 @@
+from src import engine
+
+import time
+
+# cycle the engine once per second
+if __name__ == "__main__":
+    try:
+        while True:
+            engine.cycle()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping the engine.")
